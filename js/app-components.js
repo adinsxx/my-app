b23159a0f3d510e7f8bfca737960d4de66c742ed
Connor <c.mccurrie@gmail.com>
2021-10-18 11:27:10 -0500

    Fixed Navbar/Adding Cards

--- js/app-components.js
+++ js/app-components.js
@@ -1,4 +1,48 @@
-const NavComponent = Vue.component('NavComponent', {
+Vue.component('app-nav-component', {
+    data: function(){
+        return {
+
+        };
+    },
     template:
-    ``
+    `    <v-app>
+    <!--Toobar-->
+    <v-app-bar color="gray" dark dense 
+      src="https://mocah.org/thumbs/304204-Fantasy-Digital-Art-Landscape-Scenery-4K.jpg">
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Bag of Holding</v-toolbar-title>
+      <v-img gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"></v-img>
+
+
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>TBD</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>TBD</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+
+
+
+
+
+
+
+  </v-app>`
 })
--- js/app.js
+++ js/app.js
@@ -1,5 +1,52 @@
 Vue.use(Vuetify);
 
+Vue.component('app-nav-component', {
+    data: function () {
+        return {
+            items: [
+                { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+                { title: 'Photos', icon: 'mdi-image' },
+                { title: 'About', icon: 'mdi-help-box' },
+            ],
+            right: null,
+        };
+    },
+    template:
+        `    <v-app>
+    <!--Toobar-->
+    <v-app-bar color="gray" dark dense 
+      src="https://mocah.org/thumbs/304204-Fantasy-Digital-Art-Landscape-Scenery-4K.jpg">
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Bag of Holding</v-toolbar-title>
+      <v-img gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"></v-img>
+
+
+    </v-app-bar>
+
+  </v-app>`
+});
+
+Vue.component('app-card', {
+    data: function () {
+        return
+    },
+    template:
+        
+`        <v-card>
+            <v-img></v-img>
+            <v-card-title></v-card-title>
+            <v-card-text></v-card-text>
+        </v-card>`,
+
+    methods: {
+        reserve() {
+            this.loading = true
+
+            setTimeout(() => (this.loading = false), 2000)
+        },
+    }
+});
 
 const app = new Vue({
     // el: the DOM element to be replaced with a Vue instance
@@ -8,12 +55,11 @@ const app = new Vue({
         theme: {
             dark: true
         },
-    }),  
+    }),
     // data: all the data for the app
     data: () => ({
-        drawer: false,
-        group: null,
-      }),
+
+    }),
     // methods: usually "events" triggered by v-on:
     methods: {
 
@@ -23,7 +69,7 @@ const app = new Vue({
 
     },
     //mounted:  called after the instance has been mounted,
-    mounted: function() {
+    mounted: function () {
 
     },
 
